4652bbd9ce8605fc789ace18938bc7ae28218259
Anthony Miller <1anthony1@gmail.com>
2018-11-01 10:22:55 -0700

    running code

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,19 @@
-// ===============================================================================
-// DATA
-// Below data will hold all of the friends
-// ===============================================================================
-
+//Friends Data
 var friends = [
     {
       name:"Ahmed",
       photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
       scores:[
-        "5",
         "1",
         "4",
         "4",
+        "4",
         "5",
-        "1",
+        "3",
         "2",
         "5",
-        "4",
-        "1"
+        "1",
+        "5"
       ]
     },
     {
@@ -25,8 +21,8 @@ var friends = [
       photo: "https://en.wikipedia.org/wiki/Elon_Musk#/media/File:Elon_Musk_2015.jpg",
       scores: [
         "5",
-        "1",
-        "4",
+        "5",
+        "5",
         "4",
         "5",
         "1",
@@ -40,14 +36,14 @@ var friends = [
       name: "Itachi",
       photo: "https://en.wikipedia.org/wiki/Butch_Cassidy#/media/File:Butch_Cassidy_with_bowler_hat.jpg",
       scores: [
+        "2",
+        "2",
+        "5",
         "3",
         "2",
         "1",
         "3",
-        "2",
-        "1",
-        "3",
-        "2",
+        "4",
         "1",
         "5"
       ]
@@ -56,12 +52,12 @@ var friends = [
       name: "Hisoka",
       photo: "https://en.wikipedia.org/wiki/Sundance_Kid#/media/File:Sundance_Kid_and_wife-clean.jpg",
       scores: [
-        "3",
+        "5",
         "2",
         "1",
         "3",
         "2",
-        "1",
+        "2",
         "3",
         "2",
         "1",
@@ -72,11 +68,11 @@ var friends = [
       name: "Gintoki",
       photo: "https://en.wikipedia.org/wiki/Frodo_Baggins#/media/File:Elijah_Wood_as_Frodo_Baggins.png",
       scores: [
-        "5",
+        "4",
+        "4",
         "4",
         "3",
         "2",
-        "1",
         "5",
         "4",
         "3",
@@ -88,12 +84,12 @@ var friends = [
       name: "Siatama",
       photo: "https://en.wikipedia.org/wiki/Samwise_Gamgee#/media/File:Sean_Astin_as_Samwise_Gamgee.png",
       scores: [
-        "5",
-        "4",
         "3",
+        "4",
+        "2",
         "2",
         "1",
-        "5",
+        "4",
         "4",
         "3",
         "2",
@@ -102,5 +98,5 @@ var friends = [
     }
   ]
 
-// Note how we export the array. This makes it accessible to other files using require.
+// Array Export
 module.exports = friends;
--- app/routing/htmlroutes.js
+++ app/routing/htmlroutes.js
@@ -1,26 +1,16 @@
-// ===============================================================================
-// DEPENDENCIES - From Hot Restraurant Example
-// Include the path package to get the correct file path
-// ===============================================================================
+// Dependencies
 var path = require("path");
 
+/*--ROUTING--*/
+module.exports = function (app) {
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
-module.exports = function(app) {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
-
-  app.get("/survey", function(req, res) {
+  //survey
+  app.get("/survey", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
-  // If no matching route is found default to home
-  app.get("/", function(req, res) {
+  // default to home
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
